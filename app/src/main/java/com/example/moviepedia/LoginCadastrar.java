a7a92d9a2e68299de1776b75faa503395e385469
Walterharder <walter.harder@hotmail.com>
2022-10-06 19:43:15 -0300

    Início do Projeto MoviePedia

---
+++ app/src/main/java/com/example/moviepedia/LoginCadastrar.java
@@ -0,0 +1,23 @@
+package com.example.moviepedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginCadastrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_cadastrar);
+
+
+        id_login.setOnClickListener
+
+     }
+
+
+
+}
