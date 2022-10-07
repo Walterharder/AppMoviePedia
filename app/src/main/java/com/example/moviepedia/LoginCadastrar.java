5d7b88ff90a127062fc141d33fce2bcafccc45c8
Walterharder <walter.harder@hotmail.com>
2022-10-06 21:31:55 -0300

    Atualização página cadastro

--- app/src/main/java/com/example/moviepedia/LoginCadastrar.java
+++ app/src/main/java/com/example/moviepedia/LoginCadastrar.java
@@ -5,16 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginCadastrar extends AppCompatActivity {
+
+    private Button botaoLogin;
+    private Button botaoCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_cadastrar);
 
+        botaoLogin = findViewById(R.id.id_login);
 
-        id_login.setOnClickListener
+        botaoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(LoginCadastrar.this,Login.class);
+                startActivity(login);
+            }
+        });
+
+        botaoCadastrar = findViewById(R.id.id_cadastrar);
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cadastrar = new Intent(LoginCadastrar.this,Cadastrar.class);
+                startActivity(cadastrar);
+            }
+        });
 
      }
 
