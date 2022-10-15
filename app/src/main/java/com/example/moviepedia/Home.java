e9abe9a41edba7b73a448bea81aceae1731542f9
Walterharder <walter.harder@hotmail.com>
2022-10-14 22:23:58 -0300

    Novas telas de navegação

--- app/src/main/java/com/example/moviepedia/Home.java
+++ app/src/main/java/com/example/moviepedia/Home.java
@@ -1,25 +1,27 @@
 package com.example.moviepedia;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.moviepedia.databinding.ActivityHome2Binding;
+import com.google.android.material.navigation.NavigationView;
 
 public class Home extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityHome2Binding binding;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +37,15 @@ public class Home extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_sobre, R.id.nav_filmes)
+                R.id.nav_home, R.id.nav_sobre, R.id.nav_filmes, R.id.nav_series, R.id.nav_desenhos, R.id.nav_shows,R.id.nav_documentarios)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+      
+
     }
 
     @Override
@@ -56,4 +61,6 @@ public class Home extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
 }
